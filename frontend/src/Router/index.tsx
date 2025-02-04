120bd6f00fb27d44f70ce7d8aa23091512592b1f
Thomas Soares <thomas__soares@hotmail.com>
2025-02-03 23:42:10 -0300

    add routes

---
+++ frontend/src/Router/index.tsx
@@ -0,0 +1,10 @@
+
+
+export function Router( {
+    return (
+        <BrowserRouter>
+            <Routte path="/login" element={<h1>Login</h1>}
+            <Routte path="/register" element={<h1>Register</h1>}
+        </BrowserRouter>
+    )
+})
