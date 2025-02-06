27672e1df57937e4357232eb586f841be5d76905
Thomas Soares <thomas__soares@hotmail.com>
2025-02-05 23:51:41 -0300

    router adjusts

--- frontend/src/Router/index.tsx
+++ frontend/src/Router/index.tsx
@@ -1,10 +1,11 @@
+import { BrowserRouter, Route } from "react-router-dom";
 
 
-export function Router( {
+export function Router() {
     return (
         <BrowserRouter>
-            <Routte path="/login" element={<h1>Login</h1>}
-            <Routte path="/register" element={<h1>Register</h1>}
+            <Route path="/login" element={<h1>Login</h1>} />
+            <Route path="/register" element={<h1>Register</h1>} />
         </BrowserRouter>
     )
 })
