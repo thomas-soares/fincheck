796338a3aecb3ebc6551cdf2503d33a2e018361b
Thomas Soares <thomas__soares@hotmail.com>
2025-02-12 22:38:54 -0300

    add Logo

--- frontend/src/view/layouts/AuthLayout.tsx
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -1,7 +1,11 @@
+import { Logo } from '../components/Logo'
+
 export function AuthLayout() {
     return (
         <div className="flex w-full h-full">
-            <div className="w-1/2 h-full flex items-center"></div>
+            <div className="w-1/2 h-full flex items-center">
+            <Logo className="h-6 text-gray-500" />
+            </div>
         </div>
     )
 }
