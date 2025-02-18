a0b1720633f89fea1f832c376f9dfb8989d64b0f
Thomas Soares <thomas__soares@hotmail.com>
2025-02-17 23:51:34 -0300

    add illustration

--- frontend/src/view/layouts/AuthLayout.tsx
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -4,9 +4,12 @@ export function AuthLayout() {
     return (
         <div className="flex w-full h-full">
             <div className="w-1/2 h-full flex items-center">
-            <Logo className="h-6 text-gray-500" />
+                <Logo className="h-6 text-gray-500" />
 
-            <div className='mt-16 bg-red-500 w-full'><h1>Pagina...</h1></div>
+                <div className='mt-16 bg-red-500 w-full'><h1>Pagina...</h1></div>
+            </div>
+            <div>
+                <img src={illustration} alt="" />
             </div>
         </div>
     )
