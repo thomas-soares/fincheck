ddbf6c9ba2135ac370d1ca3a6b48cb245ac49c45
Thomas Soares <thomas__soares@hotmail.com>
2025-02-20 23:55:52 -0300

    adjust layout size

--- frontend/src/view/layouts/AuthLayout.tsx
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -6,7 +6,9 @@ export function AuthLayout() {
             <div className="w-1/2 h-full flex items-center">
                 <Logo className="h-6 text-gray-500" />
 
-                <div className='mt-16 bg-red-500 w-full'><h1>Pagina...</h1></div>
+                <div className='mt-16 bg-red-500'>
+                    <h1>Pagina...</h1>
+                </div>
             </div>
             <div>
                 <img src={illustration} className="object-cover w-full h-full" alt="" />
