e5f341167ecf57aec78b895e978a957136a5d0aa
Thomas Soares <thomas__soares@hotmail.com>
2025-03-30 23:35:38 -0300

    remove content

--- frontend/src/view/pages/Dashboard/components/Accounts.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts.tsx
@@ -1,11 +1,5 @@
 export function Accounts() {
     return (
-        <div>
-            <div>
-                <span>Saldo total</span>
-
-                
-            </div>
-        </div>
+        
     )
 }
