b447edd9c93261c8ec0c6b348a0ccf561959fffe
Thomas Soares <thomas__soares@hotmail.com>
2025-04-08 23:52:43 -0300

    init login layout

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -1,5 +1,9 @@
 export function Login() {
     return (
-        
+        <div>
+            <header className="flex flex-col items-center gap-4">
+                <h1 className="text-2xl font-bold">Entre em sua conta</h1>
+            </header>
+        </div>
     )
 }
