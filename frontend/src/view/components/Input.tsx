761a9f7f486ef83cb02505355fef00ce1966721c
Thomas Soares <thomas__soares@hotmail.com>
2025-04-11 23:50:22 -0300

    add label

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -2,6 +2,10 @@ export function Input({ placeholder, ...props }: InputProps) {
     return (
         <div className="relative">
             <input {...props} className="bg-white w-full rounded-lg border" />
+        
+            <label className="absolute left-3">
+                {placeholder}
+            </label>
         </div>
     )
 }
