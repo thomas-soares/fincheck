2ba7d1c1d1fa7f0ad9cdba7879d38a32cf8cf30a
Thomas Soares <thomas__soares@hotmail.com>
2025-04-12 23:43:44 -0300

    input style

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,9 +1,9 @@
 export function Input({ placeholder, ...props }: InputProps) {
     return (
         <div className="relative">
-            <input {...props} className="bg-white w-full rounded-lg border" />
+            <input {...props} className="bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px]" />
         
-            <label className="absolute left-3">
+            <label className="absolute left-[13px] top-3.5">
                 {placeholder}
             </label>
         </div>
