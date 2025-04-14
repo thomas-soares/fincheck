ef9e3d0dd603e833080194138a7a1d37b3dfb6a6
Thomas Soares <thomas__soares@hotmail.com>
2025-04-13 23:54:58 -0300

    add attributes

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,9 +1,9 @@
-export function Input({ placeholder, ...props }: InputProps) {
+export function Input({ placeholder, name, ...props }: InputProps) {
     return (
         <div className="relative">
-            <input {...props} className="bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px]" />
+            <input {...props} name={name} className="bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px]" />
         
-            <label className="absolute left-[13px] top-3.5">
+            <label htmlFor={name} className="absolute left-[13px] top-3.5">
                 {placeholder}
             </label>
         </div>
