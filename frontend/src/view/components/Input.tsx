d68dc4394465ecaa15b961b3800c7312fafdb0b4
Thomas Soares <thomas__soares@hotmail.com>
2025-04-15 23:51:25 -0300

    add inputId

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,7 +1,7 @@
 export function Input({ placeholder, name, ...props }: InputProps) {
     return (
         <div className="relative">
-            <input {...props} name={name} className="bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800" />
+            <input {...props} name={name} id={inputId} className="bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800" />
         
             <label htmlFor={name} className="absolute left-[13px] top-3.5 pointer">
                 {placeholder}
