0c8b115943ede02c198b272ce2733501d2795641
Thomas Soares <thomas__soares@hotmail.com>
2025-04-19 23:29:32 -0300

    change id

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -3,7 +3,7 @@ export function Input({ placeholder, name, ...props }: InputProps) {
         <div className="relative">
             <input {...props} name={name} id={inputId} className="bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800" />
         
-            <label htmlFor={name} className="absolute left-[13px] top-3.5 pointer">
+            <label htmlFor={inputId} className="absolute left-[13px] top-3.5 pointer">
                 {placeholder}
             </label>
         </div>
