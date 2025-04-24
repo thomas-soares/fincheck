6be53d2ec1c9bc3e124ceec70ea29ab26afcf93c
Thomas Soares <thomas__soares@hotmail.com>
2025-04-23 22:18:17 -0300

    header register page

--- frontend/src/view/pages/Register/index.tsx
+++ frontend/src/view/pages/Register/index.tsx
@@ -1,5 +1,13 @@
 export function Register() {
     return (
-        <>Crie sua conta</>
+        <>
+        <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold text-gray-900">
+
+        Crie sua conta
+        </h1>
+
+        </header>
+        </>
     )
 }
