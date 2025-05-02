ea1f2fcaa526b725685ee262043b6bfd2c17ba1e
Thomas Soares <thomas__soares@hotmail.com>
2025-05-01 23:43:17 -0300

    create useLoginController

--- frontend/src/view/pages/Login/useLoginController.ts
+++ frontend/src/view/pages/Login/useLoginController.ts
@@ -2,5 +2,7 @@ const schema = z.object({
     email: z.string()
     .nonempty('E-mail é obrigatório')
     .email('Informe um e-mail válido'),
-    
+
 })
+
+type FormData = z.infer<typeof schema>;
