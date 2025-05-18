7c9f183e73bc65c08475dd8c32e39aca566f3372
Thomas Soares <thomas__soares@hotmail.com>
2025-05-17 23:52:42 -0300

    add sign route

--- frontend/src/app/services/httpClient.ts
+++ frontend/src/app/services/httpClient.ts
@@ -4,5 +4,7 @@ const httpClient = axios.create({
     baseURL: 'http://localhost:3000'
 })
 
+const { data } = await axios.post('http://localhost:3000/auth/signin')
+
 httpClient.post('http://localhost:3000/auth/signin')
 httpClient.get('http://localhost:3000/')
