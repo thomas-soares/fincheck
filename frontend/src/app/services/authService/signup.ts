243660d6319a98b9d68602634f2b5a70265d9c1e
Thomas Soares <thomas__soares@hotmail.com>
2025-05-20 23:33:02 -0300

    edit params

--- frontend/src/app/services/authService/signup.ts
+++ frontend/src/app/services/authService/signup.ts
@@ -1,3 +1,9 @@
+interface SignupParams {
+    name: string;
+    email: string;
+    password: string
+}
+
 async function signup(params: SignupParams) {
     const { data } = await httpClient.post<{ accessToken: string }>
 
