b232f1249452bb5f39f07ed90fcf63e7cf7c59d8
Thomas Soares <thomas__soares@hotmail.com>
2025-05-26 23:52:45 -0300

    login form

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -7,6 +7,9 @@ export function Login() {
                     <span>Novo por aqui?</span>
                 </p>
             </header>
+            <form onSubmit={handleSubmit} className="mt-[60px]">
+
+            </form>
         </div>
     )
 }
