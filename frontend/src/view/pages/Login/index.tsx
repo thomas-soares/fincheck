91878ec8224e9d74643bafc8047c82af631f28b9
Thomas Soares <thomas__soares@hotmail.com>
2025-05-30 23:54:12 -0300

    import login controller

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -10,7 +10,7 @@ export function Login() {
                 </p>
             </header>
             <form onSubmit={handleSubmit} className="mt-[60px] flex flex-col gap-4">
-<Input type="email" />
+<Input type="email" placeholder="E-mail"  />
             </form>
         </div>
     )
