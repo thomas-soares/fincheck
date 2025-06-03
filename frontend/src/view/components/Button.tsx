ce171800924c90c221e43bee5203552cad1296d1
Thomas Soares <thomas__soares@hotmail.com>
2025-06-02 23:56:32 -0300

    button props

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -1,6 +1,6 @@
 interface ButtonProps extends ComponentProps<'button'> {}
 
-export function Button(props: ButtonProps) {
+export function Button({ className, ...props}: ButtonProps) {
     return (
         <button {...props} className={cn("bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100")}></button>
     )
